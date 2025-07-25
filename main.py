87af7a6f37da6cc08eb7a3fe6036654e5496477f
Joao Paulo Barbosa <jpbgois10@gmail.com>
2025-07-25 12:36:56 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import sys
+
+import pygame
+
+pygame.init()
+window = pygame.display.set_mode(size=(600, 480))
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
