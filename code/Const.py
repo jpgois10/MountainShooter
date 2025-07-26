95997acd536295ac9e80f7fe617b37493c49866f
Joao Paulo Barbosa <jpbgois10@gmail.com>
2025-07-26 11:34:37 -0300

    Creating const file

---
+++ code/Const.py
@@ -0,0 +1,17 @@
+# C
+COLOR_ORANGE = (255, 128, 0)
+COLOR_YELLOW = (255, 255, 128)
+COLOR_WHITE = (255, 255, 255)
+COLOR_GREEN = (0, 128, 0)
+COLOR_CYAN = (0, 128, 128)
+
+# M
+MENU_OPTION = ('NEW GAME 1P',
+               'NEW GAME 2P - COOPERATIVE',
+               'NEW GAME 2P - COMPETITIVE',
+               'SCORE',
+               'EXIT')
+
+# W
+WIN_WIDTH = 576
+WIN_HEIGHT = 324
